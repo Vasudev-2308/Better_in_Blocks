f3adc8d2c105b309fbf874ccddfa1c11f1d69452
Vasudev-2308 <kingzvasu660@gmail.com>
2021-03-01 11:41:34 +0530

    Firebase Add

--- frontend/lib/screens/login.dart
+++ frontend/lib/screens/login.dart
@@ -32,12 +32,13 @@ class _LoginState extends State<Login> {
 
                     end: Alignment(
                         0.9, 0.0), // 10% of the width, so there are ten blinds.
-                    // colors: [
-                    //   const Color(0xff6DD5FA),
-                    //   const Color(0xff86A8E7),
-                    //   const Color(0xffffffff)
-                    // ], // red to yellow
-                    // repeats the gradient over the canvas
+                    colors: [
+                      const Color(0xff6DD5FA),
+                      const Color(0xff86A8E7),
+                      const Color(0xffffffff)
+                    ], // red to yellow
+                    tileMode: TileMode
+                        .repeated, // repeats the gradient over the canvas
                   ),
                 ),
               ),
